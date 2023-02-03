78366429dc03f0ec5db5c58e1c0204a52143f3c6
Junior Zabala <juniorzv70@gmail.com>
2023-02-03 09:51:43 -0500

    Remover dropdown

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -152,15 +152,7 @@
               <div class="avatar flex-shrink-0">
                 <img src="{{asset('assets/img/icons/unicons/cc-primary.png')}}" alt="Credit Card" class="rounded">
               </div>
-              <div class="dropdown">
-                <button class="btn p-0" type="button" id="cardOpt1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="bx bx-dots-vertical-rounded"></i>
-                </button>
-                <div class="dropdown-menu" aria-labelledby="cardOpt1">
-                  <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                  <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                </div>
-              </div>
+
             </div>
             <span class="fw-semibold d-block mb-1">Transactions</span>
             <h3 class="card-title mb-2">$14,857</h3>
