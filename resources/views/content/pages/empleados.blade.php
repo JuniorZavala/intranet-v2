2a5fe91c5169b700d17907e53a69ae49f490dd33
Junior Zabala <juniorzv70@gmail.com>
2023-02-06 11:12:17 -0500

    modificaciones en los botones de la tabla

--- resources/views/content/pages/empleados.blade.php
+++ resources/views/content/pages/empleados.blade.php
@@ -21,7 +21,7 @@
           <th></th>
         </tr>
       </thead>
-      <tbody class="table-border-bottom-0">
+      <tbody class="table-border-bottom-0" style="font-size:14px">
         @foreach ( $empleados as $empleado )
         <tr class="table-default text-capitalize">
           <td><i class="tf-icons bx bx-user fa-lg me-1"></i> {{$empleado->nombre}} {{$empleado->ape_pat}} {{$empleado->ape_mat}}</td>
@@ -30,13 +30,19 @@
           <td><span class="badge bg-label-primary me-1">{{$empleado->area->nombre}}</span></td>
           <td>{{$empleado->empresa->razon_social}}</td>
           <td>
-            <div class="dropdown">
+          <a href="{{ route('empleado-show', $empleado->id)}}" class="btn btn-icon btn-primary">
+                <span class="tf-icons bx bx-pie-chart-alt"></span>
+          </a>
+          <a href="{{ route('empleado-show', $empleado->id)}}" class="btn btn-icon btn-danger">
+            <span class="tf-icons bx bx-pie-chart-alt"></span>
+          </a>
+           <!--/  <div class="dropdown">
               <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="bx bx-dots-vertical-rounded"></i></button>
               <div class="dropdown-menu">
                 <a class="dropdown-item" href="{{ route('empleado-show', $empleado->id)}}"><i class="bx bx-edit-alt me-1"></i> Edit</a>
                 <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-1"></i> Delete</a>
               </div>
-            </div>
+            </div> -->
           </td>
         </tr>
         @endforeach
